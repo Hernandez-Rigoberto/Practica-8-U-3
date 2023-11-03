ccee52b08161f406d44de4659088c9193a508413
ASUS-PC <ASUS-PC@RIGOBERTO>
2023-11-03 01:54:38 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+namespace Practica_8_U_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int edad;
+            edad = int.Parse(textBox1.Text);
+
+            if (edad < 12.0) MessageBox.Show(" NIÑO ");
+            else
+                if (edad < 18.0) MessageBox.Show(" ADOLESCENTE ");
+            else
+                if (edad < 60.0) MessageBox.Show(" ADULTO ");
+            else
+                if (edad > 60.0) MessageBox.Show(" ADULTO MAYOR ");
+        }
+    }
+}
